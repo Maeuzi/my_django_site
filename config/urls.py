ee4a64918837707c5ca9172ca3f6aee833af6b8a
Sweta Nancy <nancysh1905@gmail.com>
2026-02-05 19:52:00 +0530

    removed name and added basket

--- config/urls.py
+++ config/urls.py
@@ -1,29 +1,51 @@
-from django.templatetags.static import static
+from django.contrib import admin
+from django.urls import path
 from django.http import HttpResponse
 
-
-
+def home(request):
     html_content = """
     <!DOCTYPE html>
     <html>
     <head>
         <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
         <style>
-            body { background-color: #f0f2f5; font-family: 'Press Start 2P', cursive; text-align: center; }
-            .grid { display: grid; grid-template-columns: repeat(2, 100px); gap: 10px; justify-content: center; }
-            .card { width: 100px; height: 100px; border: 3px solid black; background: white; }
-            img { width: 80px; display: block; margin: auto; }
+            body { 
+                background-color: #ff9ff3; 
+                font-family: 'Press Start 2P', cursive; 
+                text-align: center; 
+                padding: 50px; 
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                margin: 0;
+            }
+            .box { 
+                border: 4px solid black; 
+                background: white; 
+                padding: 40px; 
+                display: inline-block; 
+                box-shadow: 10px 10px 0px #000; 
+            }
+            .icons {
+                font-size: 60px;
+                margin: 20px 0;
+            }
+            p { font-size: 12px; line-height: 1.5; }
         </style>
     </head>
     <body>
-        <h1>SWETA'S PUZZLE</h1>
-        <div class="grid">
-            <div class="card"><img src='""" + luffy + """'></div>
-            <div class="card"><img src='""" + kitty + """'></div>
-            <div class="card"><img src='""" + tomato + """'></div>
-            <div class="card"><img src='""" + luffy + """'></div>
+        <div class="box">
+            <p>PUZZLE LOADING...</p>
+            <div class="icons">👒 🧺 🍅</div>
+            <p>頑張れ！</p>
         </div>
     </body>
     </html>
     """
     return HttpResponse(html_content)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home),
+]
